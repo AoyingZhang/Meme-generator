af7bcbd27b4244ab444af4c29c6727daa5d6dcbc
Aoying Zhang <107318892+AoyingZhang@users.noreply.github.com>
2023-05-26 15:30:02 -0400

    Create Meme.js

---
+++ Components/Meme.js
@@ -0,0 +1,37 @@
+import React from "react"
+import memesData from "../memesData.js"
+
+export default function Meme() {
+    
+    let url
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url = memesArray[randomNumber].url
+    }
+    
+    return (
+        <main>
+            <p>{url}</p>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image
+                </button>
+            </div>
+        </main>
+    )
+}
